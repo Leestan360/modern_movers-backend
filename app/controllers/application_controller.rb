14f3045326fc0c76264d4927c0308341c709c12d
Leestan360 <stanleymayore2@gmail.com>
2022-09-07 09:03:15 +0300

    Create a post request route for new users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,10 @@ class ApplicationController < Sinatra::Base
     users.to_json
   end
 
+  post '/users' do 
+    user = User.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email],
+    password: params[:password], password_confirmation: params[:password_confirmation])
+    user.to_json
+  end
+
 end
