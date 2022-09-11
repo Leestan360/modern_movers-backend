0aa77057a45585426476977972563b8d75dae213
Leestan360 <stanleymayore2@gmail.com>
2022-09-11 17:37:15 +0300

    Add booking instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,8 @@ puts "Planting seed"
 stanley = User.create(first_name: "Stanley", last_name: "Mayore", email: "stan@gmail.com", password: "12345678", password_confirmation: "12345678")
 
 # Booking instances
-booking = Booking.create(body: "help me move to area 1", user: stanley)
-booking1 = Booking.create(body: "help me move to area 2", user: stanley)
+booking = Booking.create(body: "help me move to Kenyatta Road", user: stanley)
+booking1 = Booking.create(body: "help me move to Ruaka", user: stanley)
+booking2 = Booking.create(body: "help me move to Thika", user: stanley)
 
 puts "Done!"
